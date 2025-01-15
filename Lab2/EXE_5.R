22d43fbaca5fcd42e5af9310006f1baba8bc09e8
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2025-01-15 13:00:27 +0530

    Update EXE_5.R

--- Lab2/EXE_5.R
+++ Lab2/EXE_5.R
@@ -1,1 +1,23 @@
+y = 1:24
+dim(y) = c(2, 4, 3)
+y
+X <- matrix(c(1, 0, 0, 0, 1, 0, 0, 0, 1), nrow = 3)
+X
+vector <- c(1, 2, 3, 4, 4, 3, 2, 1)
+V <- matrix(vector, byrow = T, nrow = 2)
+V
+vector <- c(1, 2, 3, 4, 4, 3, 2, 1)
+V <- matrix(vector, byrow = F, nrow = 2)
+V
+dim(vector) <- c(4, 2)
+vector
+is.matrix(vector)
+x = c(1, 2, 3, 4, "AB", "R")
+dim(x) = c(3, 2, 1)
+x
+vec = 1:64
+dim(vec) = c(2, 2, 4, 4)
+vec
+vec[1, 1, 2, 4]
+vec[,,1,1]
 
